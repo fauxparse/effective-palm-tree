c179a47e0859c752ed8c6bb0c2de2715cba4f99f
Matt Powell <fauxparse@gmail.com>
2017-03-07 16:44:42 +1300

    specs for users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) { create(:user, email: 'ripley@sula.co') }
+
+  it { is_expected.to be_valid }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+end
