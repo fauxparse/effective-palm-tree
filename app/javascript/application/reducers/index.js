8eb8678db5b7968db81825c344f76bdaaa29287d
Matt Powell <fauxparse@gmail.com>
2017-03-12 09:59:17 +1300

    remove coupling in CalendarMonth

--- app/javascript/application/reducers/index.js
+++ app/javascript/application/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer as routing } from 'react-router-redux'
+import calendar from './calendar'
 import events from './events'
 import groups from './groups'
 import user from './user'
 
-export default combineReducers({ events, groups, routing, user })
+export default combineReducers({ calendar, events, groups, routing, user })
