fd5e304acb30768477137dc9890403b2e0774130
Matt Powell <fauxparse@gmail.com>
2017-04-09 15:26:31 +1200

    stackable views

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,18 @@
+class GroupsController < ApplicationController
+  def index
+  end
+
+  def show
+    respond_to do |format|
+      format.json do
+        render json: membership, include: { group: [:members, :roles] }
+      end
+    end
+  end
+
+  private
+
+  def membership
+    @membership ||= group(:id).members.find_by(user_id: current_user.id)
+  end
+end
